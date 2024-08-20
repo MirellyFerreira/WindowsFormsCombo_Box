2690d3ab997ead572724871c8dae3b2ffba81915
Usuario <Usuario@CPD02-M14>
2024-08-20 12:00:11 -0300

    Adicionar arquivos de projeto.

---
+++ WindowsFormsCombo_Box/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace WindowsFormsCombo_Box
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTexto = new System.Windows.Forms.TextBox();
+            this.btnInserir = new System.Windows.Forms.Button();
+            this.btnOrdenar = new System.Windows.Forms.Button();
+            this.cboLista = new System.Windows.Forms.ComboBox();
+            this.btnRemover = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblQtde = new System.Windows.Forms.Label();
+            this.btnExibir = new System.Windows.Forms.Button();
+            this.txtIndice = new System.Windows.Forms.TextBox();
+            this.txtConteudo = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(120, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Texto:";
+            // 
+            // txtTexto
+            // 
+            this.txtTexto.Location = new System.Drawing.Point(193, 34);
+            this.txtTexto.Name = "txtTexto";
+            this.txtTexto.Size = new System.Drawing.Size(354, 20);
+            this.txtTexto.TabIndex = 1;
+            // 
+            // btnInserir
+            // 
+            this.btnInserir.Location = new System.Drawing.Point(240, 93);
+            this.btnInserir.Name = "btnInserir";
+            this.btnInserir.Size = new System.Drawing.Size(91, 47);
+            this.btnInserir.TabIndex = 2;
+            this.btnInserir.Text = "Inserir no Combo";
+            this.btnInserir.UseVisualStyleBackColor = true;
+            this.btnInserir.Click += new System.EventHandler(this.btnInserir_Click);
+            // 
+            // btnOrdenar
+            // 
+            this.btnOrdenar.Location = new System.Drawing.Point(376, 93);
+            this.btnOrdenar.Name = "btnOrdenar";
+            this.btnOrdenar.Size = new System.Drawing.Size(85, 47);
+            this.btnOrdenar.TabIndex = 3;
+            this.btnOrdenar.Text = "Ordenar o Combo";
+            this.btnOrdenar.UseVisualStyleBackColor = true;
+            this.btnOrdenar.Click += new System.EventHandler(this.btnOrdenar_Click);
+            // 
+            // cboLista
+            // 
+            this.cboLista.FormattingEnabled = true;
+            this.cboLista.Location = new System.Drawing.Point(193, 165);
+            this.cboLista.Name = "cboLista";
+            this.cboLista.Size = new System.Drawing.Size(354, 21);
+            this.cboLista.TabIndex = 4;
+            // 
+            // btnRemover
+            // 
+            this.btnRemover.Location = new System.Drawing.Point(321, 205);
+            this.btnRemover.Name = "btnRemover";
+            this.btnRemover.Size = new System.Drawing.Size(92, 41);
+            this.btnRemover.TabIndex = 5;
+            this.btnRemover.Text = "Remover do Combo";
+            this.btnRemover.UseVisualStyleBackColor = true;
+            this.btnRemover.Click += new System.EventHandler(this.btnRemover_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(165, 277);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Quantidade de Itens:";
+            // 
+            // lblQtde
+            // 
+            this.lblQtde.AutoSize = true;
+            this.lblQtde.Location = new System.Drawing.Point(274, 277);
+            this.lblQtde.Name = "lblQtde";
+            this.lblQtde.Size = new System.Drawing.Size(0, 13);
+            this.lblQtde.TabIndex = 7;
+            // 
+            // btnExibir
+            // 
+            this.btnExibir.Location = new System.Drawing.Point(321, 328);
+            this.btnExibir.Name = "btnExibir";
+            this.btnExibir.Size = new System.Drawing.Size(102, 49);
+            this.btnExibir.TabIndex = 8;
+            this.btnExibir.Text = "Exibir Dados do Combo";
+            this.btnExibir.UseVisualStyleBackColor = true;
+            this.btnExibir.Click += new System.EventHandler(this.btnExibir_Click);
+            // 
+            // txtIndice
+            // 
+            this.txtIndice.Location = new System.Drawing.Point(193, 394);
+            this.txtIndice.Name = "txtIndice";
+            this.txtIndice.Size = new System.Drawing.Size(354, 20);
+            this.txtIndice.TabIndex = 9;
+            // 
+            // txtConteudo
+            // 
+            this.txtConteudo.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.txtConteudo.Location = new System.Drawing.Point(193, 453);
+            this.txtConteudo.Name = "txtConteudo";
+            this.txtConteudo.Size = new System.Drawing.Size(354, 20);
+            this.txtConteudo.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(97, 397);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Índice do Combo:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(97, 460);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(85, 13);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Texto do Combo";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(120, 173);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(32, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Lista:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 598);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtConteudo);
+            this.Controls.Add(this.txtIndice);
+            this.Controls.Add(this.btnExibir);
+            this.Controls.Add(this.lblQtde);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnRemover);
+            this.Controls.Add(this.cboLista);
+            this.Controls.Add(this.btnOrdenar);
+            this.Controls.Add(this.btnInserir);
+            this.Controls.Add(this.txtTexto);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTexto;
+        private System.Windows.Forms.Button btnInserir;
+        private System.Windows.Forms.Button btnOrdenar;
+        private System.Windows.Forms.ComboBox cboLista;
+        private System.Windows.Forms.Button btnRemover;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblQtde;
+        private System.Windows.Forms.Button btnExibir;
+        private System.Windows.Forms.TextBox txtIndice;
+        private System.Windows.Forms.TextBox txtConteudo;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ WindowsFormsCombo_Box/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsCombo_Box
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInserir_Click(object sender, EventArgs e)
+        {
+            cboLista.Items.Add(txtTexto.Text);
+            lblQtde.Text = cboLista.Items.Count.ToString();
+            txtTexto.Clear();
+            txtTexto.Select();
+        }
+
+        private void btnOrdenar_Click(object sender, EventArgs e)
+        {
+            cboLista.Sorted = true;
+        }
+
+        private void btnRemover_Click(object sender, EventArgs e)
+        {
+            if (cboLista.SelectedIndex != -1)
+            {
+                cboLista.Items.RemoveAt(cboLista.SelectedIndex);
+                lblQtde.Text = cboLista.Items.Count.ToString();
+                cboLista.ResetText();
+            }
+        }
+
+        private void btnExibir_Click(object sender, EventArgs e)
+        {
+            if (cboLista.SelectedIndex != -1)
+            {
+                txtConteudo.Text = cboLista.SelectedItem.ToString();
+                txtConteudo.Text = cboLista.SelectedItem?.ToString();
+            }
+        }
+    }
+}
+
